3f6e6182619145bf5d0c57a36dc220a905e32975
Chisom Emmanuel <mailemmanuel00@gmail.com>
2024-02-08 13:13:30 +0100

    fix

--- src/app/add-product/page.jsx
+++ src/app/add-product/page.jsx
@@ -5,18 +5,18 @@
 import OverviewLayout from '@/layout/UserLayout'
 import ProductForm from '@/views/AddProduct/form'
 import Link from 'next/link'
-import ChevronLeftRoundedIcon from '@mui/icons-material/ChevronLeftRounded'
+import ArrowBackIosNewOutlinedIcon from '@mui/icons-material/ArrowBackIosNewOutlined'
 
 export default function AddProduct() {
   return (
     <OverviewLayout>
-      <div className='flex items-center gap-10 '>
-        <Link href='products'>
-          <span >
-            <ChevronLeftRoundedIcon className='font-semibold text-6xl text-indigo-600'/>
-          </span>
-        </Link>
-        <p className=' text-indigo-600 text-2xl font-semibold'>Add Product</p>
+      <div>
+        <p className='flex items-center gap-10 text-indigo-600 text-xl font-semibold'>
+          <Link href='products'>
+            <ArrowBackIosNewOutlinedIcon />
+          </Link>
+          Add Product
+        </p>
       </div>
 
       <ProductForm />
