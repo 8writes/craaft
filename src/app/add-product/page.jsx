e604b3f15c9bff3a3dbf464fc3aaba1e2698ab5e
Chisom Emmanuel <mailemmanuel00@gmail.com>
2024-02-08 12:47:12 +0100

    fix

--- src/app/add-product/page.jsx
+++ src/app/add-product/page.jsx
@@ -12,13 +12,9 @@ export default function AddProduct() {
     <OverviewLayout>
       <div className='flex items-center gap-10 '>
         <Link href='products'>
-          <p className='font-semibold text-4xl text-indigo-600'>
-            <ChevronLeftRoundedIcon />
-          </p>
+          <ChevronLeftRoundedIcon className='font-semibold text-6xl text-indigo-600' />
         </Link>
-        <p className=' text-indigo-600 text-2xl font-semibold'>
-          Add Product
-        </p>
+        <p className=' text-indigo-600 text-2xl font-semibold'>Add Product</p>
       </div>
 
       <ProductForm />
